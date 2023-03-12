936056e708a924345b52699e1023089715f9eb35
mdnriyas17 <123050293+mdnriyas17@users.noreply.github.com>
2023-03-12 13:28:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const picma = document.getElementById("madh");
+picma.innerHTML=""
+
+const timemadh = setTimeout(madhu, 2000);
+
+function madhu(){
+    document.getElementById("madh").innerHTML= "<h1>happy birthday duplicate doctor!!!";
+}
+
+const summa = document.getElementById("madh");
+
+picma.append(summa);
